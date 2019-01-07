3c903bde1623166bc0e2fd7035d37f6b6370dd5c
Chun <zhengc42@gmail.com>
2019-01-06 22:16:40 -0500

    initial commit frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PillowFight/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PillowFight from './components/PillowFight.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      PillowFight
   }
 }
 </script>
---
+++ frontend/src/components/PillowFight.vue
@@ -0,0 +1,20 @@
+<template>
+<p> hello </p>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    name: 'PillowFight',
+    beforeCreate() {
+            axios.get('http://localhost:8080/api/User', {
+        headers: {"Access-Control-Allow-Origin": "*"}
+        }).then(response => {
+            this.users = response.data;
+        }).catch(e => {
+            this.errors = e 
+        })
+    },
+}
+</script>
+<style>
+</style>
